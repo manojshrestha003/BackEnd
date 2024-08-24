b3cbba1fa851a4ae9f4ae03e4f44b8b3e8211ef4
manojshrestha003 <manoj.shrestha2081@gmail.com>
2024-08-24 15:56:55 +0545

    user controller update

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -314,6 +314,81 @@ const loginUser = AsyncHandler( async (req, res)=>{
   })
 
 
+  const getUserChannelProfile = AsyncHandler(async(req, res)=>{
+     const { username}= req.params
+     if(!username?.trim()){
+      throw new ApiError(400, "username is missing ")
+
+     }
+     const channel = await User.aggregate([
+      {
+        $match :{
+          username : username?.toLowerCase()
+        }
+      },
+      {
+        $lookup :{
+          from : "subscription",
+          localField: "_id",
+          foreignField: "channel",
+          as : "subscribers"
+        }
+      },{
+        $lookup : {
+          from : "subscription",
+          localField: "_id",
+          foreignField: "subscriber",
+          as : "subscribedTo "
+
+        }
+      },
+      {
+        $addFields: {
+          subscribersCount: {
+            $size:  "$subscribers"
+          },
+          channelsSubscribedToCount:{
+            $size: "$subscribedTo "
+          },
+          isSubscribed: {
+            $cond:{
+              if: {$in: [req.user?._id, "$subscribers.subscriber"]},
+              then: true,
+              else: false
+            }
+          }
+          
+
+        }
+      },
+      {
+        $project: {
+          fullname: 1,
+          username: 1,
+          subscribersCount: 1,
+          channelsSubscribedToCount: 1,
+          isSubscribed: 1,
+          avatar: 1,
+          coverImage: 1,
+          email: 1
+        }
+      }
+     
+     ])
+     if(!channel?.length){
+      throw new ApiError(400, "channel dosenot exists ")
+      
+
+     }
+     return res 
+     .status(200)
+     .json(
+      new ApiResponse(200, channel[0], "user channel fetched successfully ")
+     )
+
+  })
+
+
   
 export {
   registerUser, 
@@ -323,5 +398,6 @@ export {
   getCurrentuser,
   updateAccountDetails ,
   updateUserAvatar,
-  updateCoverImage
+  updateCoverImage,
+  getUserChannelProfile
 }
